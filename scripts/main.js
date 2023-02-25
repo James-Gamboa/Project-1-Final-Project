ba98b6a1d3a3fdc0a83e63388cba0132855b181a
juan sacasa <juanksacasa@gmail.com>
2023-02-24 21:07:56 -0600

    create component header

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,19 @@ function formDisplay() {
   }
 }
 
+//hamburger menu
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
 window.onload = function () {
   // @ts-ignore
   document.querySelector("main #texto").style.display = "none";
