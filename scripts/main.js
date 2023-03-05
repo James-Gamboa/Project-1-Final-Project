e1f8bb6136d483a4f6b3ad1c88b5d5c4fcdff540
James <jjguevarag@gmail.com>
2023-03-05 14:08:14 -0600

    button component update

--- scripts/main.js
+++ scripts/main.js
@@ -13,16 +13,16 @@ function formDisplay() {
 
 //hamburger menu
 const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
+const open = document.querySelector("#open");
 const cerrar = document.querySelector("#cerrar");
 
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
+open.addEventListener("click", () => {
+  nav.classList.add("visible");
+});
 
 cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
+  nav.classList.remove("visible");
+});
 
 window.onload = function () {
   // @ts-ignore
