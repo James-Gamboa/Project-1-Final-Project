70f1719c4bfba3bda39efd26889b7a49cc376903
jasonan99 <106450277+jasonan99@users.noreply.github.com>
2023-03-06 19:18:33 -0600

    add cards, images, description and hotel to destination.html (#20)

--- scripts/hidden.js
+++ scripts/hidden.js
@@ -2,19 +2,19 @@ window.onload = function () {
   // @ts-ignore
   document.querySelector("main #words").style.display = "none";
   // @ts-ignore
-  document.querySelector("main #card-container").style.display = "none";
+  document.querySelector("main #cardcontainer").style.display = "none";
   // @ts-ignore
   document.getElementById("hotel").onclick = function () {
     // @ts-ignore
     document.querySelector("main #words").style.display = "none";
     // @ts-ignore
-    document.querySelector("main #card-container").style.display = "block";
+    document.querySelector("main #cardcontainer").style.display = "block";
   };
   // @ts-ignore
   document.getElementById("detail").onclick = function () {
     // @ts-ignore
     document.querySelector("main #words").style.display = "block";
     // @ts-ignore
-    document.querySelector("main #card-container").style.display = "none";
+    document.querySelector("main #cardcontainer").style.display = "none";
   };
 };
---
+++ scripts/info.js
@@ -0,0 +1,50 @@
+const destiny = document.querySelector("#destiny");
+const words = document.querySelector("#words");
+const cardcontainer = document.querySelector("#cardcontainer");
+
+fetch("https://63f6833959c944921f7569ab.mockapi.io/Destinations")
+  .then((response) => response.json())
+  .then((data) => destination1(data[0]));
+
+fetch("https://63f6833959c944921f7569ab.mockapi.io/Hotels")
+  .then((response) => response.json())
+  .then((data) => hotel1(data[0]));
+
+function destination1(d) {
+  const div = document.createElement("div");
+  
+    div.innerHTML = `
+      <h2 class="title">${d.destination}</h2>
+      <div class="imageCont">
+      <img class="imageCont__img" src="${d.heroImage}" alt=""/>
+      </div>
+    `;
+    destiny.append(div);
+  
+  const div2 = document.createElement("div");
+  
+    div2.innerHTML = `
+      <p class="hidden" id="words">
+        ${d.description}
+      </p>
+    `;
+    words.append(div2);
+}
+
+function hotel1(h) {
+  const div3 = document.createElement("div");
+  
+    div3.innerHTML = `
+      <li class="card">
+      <div class="card__wrapper">
+      <img class="card__img" src="${h.image}" alt="">
+      <div class="card__info">
+      <p class="card__info__name">${h.hotelName}</p>
+      <p class="card__info__description">${h.description}</p>
+      </div>
+      <a class="card__cta" href=""><span class="visually-hidden">See Hotel</span></a>
+      </div>
+      </li>
+    `;
+    cardcontainer.append(div3);
+}
