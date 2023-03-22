ec03c8efb95b5d04be9844634e4c34c229621c18
Heylin-Loaiza <106451204+Heylin-Loaiza@users.noreply.github.com>
2023-03-21 20:47:57 -0600

    Update-cards (#24)
    * update destinies and hotel api
    
    * first part hotel-page
    
    * sort code
    
    sort and delete innecesary code
    
    * fix slider and pop-up menu
    
    * remove scrollbar on the slider
    
    * Update cards.js
    
    * update sliders overflow
    
    * update rooms in hotel-info.html
    
    * Fix hotel code
    
    Delete destiny header anchor
    Edit cards in rooms
    Edit buttons.scss / hotel.scss code
    paste footer in
    
    ---------
    
    Co-authored-by: jasonan99 <106450277+jasonan99@users.noreply.github.com>

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,108 +1,48 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  let slides = document.getElementsByClassName("slideshow");
-  let dots = document.getElementsByClassName("slideshow__dots--pass");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (let i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
-
 const sliderList = document.querySelector("#sliderList");
-const sliderList2 = document.querySelector("#sliderList2");
-const sliderList3 = document.querySelector("#sliderList3");
+const url = 'https://63f6833959c944921f7569ab.mockapi.io/Destinations';
+const buttonRight = document.getElementById('slideRight');
+const buttonLeft = document.getElementById('slideLeft');
+const slideshow = document.querySelectorAll('.slideshow__container')[0];
+let size = 0;
 
-for (let i = 0; i <= 2; i++) {
-  fetch("https://63f6833959c944921f7569ab.mockapi.io/Destinations")
-    .then((response) => response.json())
-    .then((data) => showDestination(data[i]));
+if (slideshow.offsetWidth > 1024) {
+  size = 725;
+} else if (slideshow.offsetWidth > 680) {
+  size = 600;
+} else {
+  size = 300;
 }
 
-for (let i = 3; i <= 5; i++) {
-  fetch("https://63f6833959c944921f7569ab.mockapi.io/Destinations")
-    .then((response) => response.json())
-    .then((data) => showDestination2(data[i]));
-}
+function next () {
+  document.getElementById('sliderList').scrollLeft += size;
+};
 
-for (let i = 6; i <= 8; i++) {
-  fetch("https://63f6833959c944921f7569ab.mockapi.io/Destinations")
-    .then((response) => response.json())
-    .then((data) => showDestination3(data[i]));
-}
+function previous () {
+  document.getElementById('sliderList').scrollLeft -= size;
+};
 
-function showDestination(d) {
-  const li = document.createElement("li");
-  li.classList.add("card");
-  li.classList.add("slideshow__fade");
-
-  li.innerHTML = `
-          <div class="card__wrapper">
-          <img class="card__img" src="${d.heroImage}" alt="">
-          <div class="card__info">
-          <p class="card__info__name">${d.destination}</p>
-          <p class="card__info__description">${d.description}</p>
-          </div>
-          <a class="card__cta" href="${d.destinyInfo}"><span class="visually-hidden">See Destination</span></a>
-          </div>
-          </li>
+fetch(url)
+.then(res => res.json())
+.then(data => {  
+  data.forEach(element => {
+    const li = document.createElement('li')
+    li.setAttribute('id', element.id)
+    li.classList.add("card");
+    li.classList.add("slideshow__fade");
+    li.innerHTML = `
+      <div class="card__wrapper">
+      <img class="card__img" src="${element.heroImage}" alt="">
+      <div class="card__info">
+      <p class="card__info__name">${element.destination}</p>
+      <p class="card__info__description">${element.description}</p>
+      </div></div>
+      </li>
       `;
-  sliderList.append(li);
-}
-
-function showDestination2(d) {
-  const li = document.createElement("li");
-  li.classList.add("card");
-  li.classList.add("slideshow__fade");
-
-  li.innerHTML = `
-          <div class="card__wrapper">
-          <img class="card__img" src="${d.heroImage}" alt="">
-          <div class="card__info">
-          <p class="card__info__name">${d.destination}</p>
-          <p class="card__info__description">${d.description}</p>
-          </div>
-          <a class="card__cta" href="${d.destinyInfo}"><span class="visually-hidden">See Destination</span></a>
-          </div>
-          </li>
-      `;
-  sliderList2.append(li);
-}
-
-function showDestination3(d) {
-  const li = document.createElement("li");
-  li.classList.add("card");
-  li.classList.add("slideshow__fade");
-
-  li.innerHTML = `
-          <div class="card__wrapper">
-          <img class="card__img" src="${d.heroImage}" alt="">
-          <div class="card__info">
-          <p class="card__info__name">${d.destination}</p>
-          <p class="card__info__description">${d.description}</p>
-          </div>
-          <a class="card__cta" href="${d.destinyInfo}"><span class="visually-hidden">See Destination</span></a>
-          </div>
-          </li>
-      `;
-  sliderList3.append(li);
-}
+    li.addEventListener('click', function() {
+      window.location.href = `../destiny.html?id=${element.id}`
+    })
+        
+    sliderList.appendChild(li)
+  })
+})
+.catch(err => console.log(err))
