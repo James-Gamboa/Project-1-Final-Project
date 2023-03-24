21d9be74d861ea5b9595770f638153b50d0b57e5
James <jjguevarag@gmail.com>
2023-03-23 18:34:09 -0600

    update the hidden.js file logic and remove the page from hotels-page.html

--- scripts/hidden.js
+++ scripts/hidden.js
@@ -1,20 +1,38 @@
 window.onload = function () {
   // @ts-ignore
-  document.querySelector("main #words").style.display = "none";
-  // @ts-ignore
   document.querySelector("main #cardcontainer").style.display = "none";
   // @ts-ignore
+  document.querySelector("main #words").style.display = "block";
+  let isHotelHidden = true;
+  // @ts-ignore
   document.getElementById("hotel").onclick = function () {
+    if (isHotelHidden) {
+      // @ts-ignore
+      document.querySelector("main #cardcontainer").style.display = "block";
+      isHotelHidden = false;
+    } else {
+      // @ts-ignore
+      document.querySelector("main #cardcontainer").style.display = "none";
+      isHotelHidden = true;
+    }
     // @ts-ignore
     document.querySelector("main #words").style.display = "none";
-    // @ts-ignore
-    document.querySelector("main #cardcontainer").style.display = "block";
   };
+
+  let isDetailHidden = false;
   // @ts-ignore
   document.getElementById("detail").onclick = function () {
-    // @ts-ignore
-    document.querySelector("main #words").style.display = "block";
+    if (!isDetailHidden) {
+      // @ts-ignore
+      document.querySelector("main #words").style.display = "none";
+      isDetailHidden = true;
+    } else {
+      // @ts-ignore
+      document.querySelector("main #words").style.display = "block";
+      isDetailHidden = false;
+    }
     // @ts-ignore
     document.querySelector("main #cardcontainer").style.display = "none";
+    isHotelHidden = true;
   };
 };
